53634b7249a2a045c93895157c3fd6b9ba4dfdf7
Daniel Clavijo <daniclavijonunez@gmail.com>
2026-02-25 13:39:00 +0100

    Finished Project
    Signed-off-by: Daniel Clavijo <daniclavijonunez@gmail.com>

--- target/reports/apidocs/type-search-index.js
+++ target/reports/apidocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"org.iis.ajnebro","l":"Calculator"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"org.iis.ajnebro","l":"Calculator"},{"p":"org.iis.ajnebro","l":"Statistics"}];updateSearchResults();
